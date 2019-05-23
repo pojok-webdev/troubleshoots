4d2a79cf688f4405011bc3b5829d772f5666fd2d
pujie <pw.prayitno@gmail.com>
2019-05-23 16:52:47 +0700

    add padiNET logo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,7 +19,12 @@ export class HomePage {
   openEnd() {
     this.menu.open('end');
   }
-
+  goToTicket(){
+    window.location.href = "/tickets"
+  }
+  goToTroubleshoot(){
+    window.location.href = "/troubleshoots"
+  }
   openCustom() {
     this.menu.enable(true, 'custom');
     this.menu.open('custom');
